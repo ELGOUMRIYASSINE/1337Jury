81d0a6efdab11f56242ea51f1de2642c79f3eb3c
Mustapha Damouh <MustaphaDamouh@outlook.com>
2026-01-22 23:37:35 +0100

    railway json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,10 +6,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
+    port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://exquisite-adaptation-production.up.railway.app/',
         changeOrigin: true,
       },
     },
